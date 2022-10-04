a50aff7134535014fd558832b17c675dc2e20dd2
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-10-04 19:49:48 +0900

    Prática de operadores em PHP

---
+++ operadores.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <title>Operacoes Aritmeticas</title>
+</head>
+<body>
+<div>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        echo "A soma vale ". ($n1+$n2);
+        echo "<br/>A subtração vale ". ($n1-$n2);
+        echo "<br/>A multiplicação vale ". ($n1*$n2);
+        echo "<br/>A divisão vale ". ($n1/$n2);
+        echo "<br/>O módulo vale ". ($n1%$n2);    
+    ?>
+</div>
+</body>
+</html>
