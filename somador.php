09fd29a9eb13f493f341593bcb678e0243d2085b
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-10-04 19:33:55 +0900

    Exercício de soma em linguagem PHP

---
+++ somador.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=“UTF-8/”>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+</head>> 
+<body>
+<div>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma entre $n1 e $n2 é igual a $s";
+    ?>
+</div>    
+</body>
+</html>    
