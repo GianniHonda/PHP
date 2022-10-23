ba2805e589eca159d07406ed512e021992ba30ab
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-10-23 20:17:30 +0900

    Cálculo de debro, cubo e raiz com o comando Switch
    O usuário pode calcular o dobro, o cubo ou a raiz quadrada de qualquer valore de sua escolha. Foi utilizada a estrutura condicional switch no PHP para a execução deste excercício.

--- exercicio01.php
+++ exercicio01.php
@@ -8,19 +8,21 @@
 <body>
 <div>
     <?php
-        $a = isset($_GET["ano"])?$_GET["ano"]:1900;
-        $i = date("Y") - $a;
-        echo "Você nasceu em $a e terá $i anos<br/>";
-        if($i >= 18) {
-            $v = "já pode votar";
-            $d = "já pode dirigir";
-        }    
-        else{
-            $v = "não pode votar";
-            $d = "não pode dirigir";
+        $n = isset($_GET["num"])?$_GET["num"]:0;
+        $o = isset($_GET["oper"])?$_GET["oper"]:1;
+        switch ($o) {
+            case 1:
+                $r = $n * 2;
+                break;
+            case 2:
+                $r = $n**3;
+                break;
+            case 3: 
+                $r = sqrt($n);        
         }
-        echo "Com essa idade você $v e também $d";
+        echo "O resultado da operação solicitada foi <span class='foco'>$r</span>"
     ?>
+    <a href="exercicio01.html" class="botao">Voltar</a>
 </div>
 </body>
 </html>
