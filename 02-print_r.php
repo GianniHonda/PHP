b8dc43896b66f35cbb96c53213ed55ec080e4752
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-11-17 12:16:17 +0900

    PHP função String print_r

---
+++ 02-print_r.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+    $v[0] = 4;
+    $v[1] = 8;
+    $v[2] = 3;
+    print_r($v);
+    $v2 = array (3,7,6,2,1,9);
+    print_r($v2);
+    ?>
+</div>
+</body>
+</html>
+ 
