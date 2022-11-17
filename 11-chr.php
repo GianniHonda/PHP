bea61c9e4de6119025fb19bda38cde88d97ca575
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-11-17 12:47:29 +0900

    PHP função String chr

---
+++ 11-chr.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $letra = chr(67);
+        echo "A letra de código 67 é $letra";
+    ?>
+</div>
+</body>
+</html>
+ 
