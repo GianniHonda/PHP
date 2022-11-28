e1fe38d02f55b4a59853f4fb2663c0ca6000dcf7
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-11-28 19:52:12 +0900

    String substr

---
+++ 22-substr.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $site = "Curso em Vídeo";
+        $sub = substr($site,0,5);
+        echo "$sub ";
+    ?>
+</div>
+</body>
+</html>
+ 
