60fd3b06705996231accc2b7f1207c23c5051cf2
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-12-01 19:50:20 +0900

    Criação de vetor com o uso de array

---
+++ 01-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <pre>
+    <?php
+        $n = array(3,5,8,2);
+        $n [] = 7;
+        print_r($n);
+    ?>
+    </pre>
+</div>
+</body>
+</html>
+ 
