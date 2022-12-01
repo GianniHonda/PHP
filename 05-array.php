9e2531b964224e937b286fb6fa6563ba9b423240
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2022-12-01 21:36:29 +0900

    Array com conteúdo vetorial personalizado

---
+++ 05-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <pre>
+    <?php
+        $v = array ("nome" => "Ana",
+                    "idade" => 23,
+                    "peso" => 65.5);
+        foreach($v as $k => $c) {
+            echo "O campo $k possui o conteúdo $c</br>";
+        }             
+    ?>
+</div>
+</body>
+</html>
+ 
